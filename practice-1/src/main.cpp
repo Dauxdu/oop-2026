0f6f9b868504943d165512f34545ebd5c1c08dd7
Dauxdu <dauxdu@yandex.ru>
2026-05-19 14:26:51 +0400

    Add initial project structure with CMake configuration and source files

---
+++ practice-1/src/main.cpp
@@ -0,0 +1,15 @@
+import std;
+
+int main(int argc, char **argv)
+{
+  if (argc > 1)
+  {
+    std::println("Привет, {}", argv[1]);
+  }
+  else
+  {
+    std::println("Привет, {}", std::getenv("USERNAME"));
+  }
+
+  return 0;
+}
