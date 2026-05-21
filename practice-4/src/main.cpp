e3b0e80bd7b4eef4a7da03427d52a1ea9be74b74
Dauxdu <dauxdu@yandex.ru>
2026-05-21 15:40:25 +0400

    feat: practice-4

--- practice-4/src/main.cpp
+++ practice-4/src/main.cpp
@@ -1,11 +1,12 @@
 import std;
 
-class fraction
+class Fraction
 {
 private:
 	int _numerator = 0;
 	int _denominator = 1;
 
+	// Поддержание инвариантов класса (I1, I2, I3)
 	void normalize()
 	{
 		if (_denominator == 0)
@@ -13,94 +14,97 @@ private:
 			_denominator = 1;
 		}
 
-		if (_denominator <= 0)
+		// I3
+		if (_denominator < 0)
 		{
 			_denominator = std::abs(_denominator);
 		}
 
+		// I1
 		if (_numerator == 0)
 		{
 			_denominator = 1;
+			return;
 		}
 
+		// I2
 		int gcd = std::gcd(_numerator, _denominator);
 		_numerator /= gcd;
 		_denominator /= gcd;
 	}
 
 public:
-	fraction(int numerator)
+	// C1
+	Fraction(int num) : _numerator(num)
 	{
-		_numerator = numerator;
-
 		normalize();
 	}
 
-	fraction(int numerator, int denominator)
+	// C2
+	Fraction(int num, int den) : _numerator(num), _denominator(den)
 	{
-		_numerator = numerator;
-		_denominator = denominator;
-
 		normalize();
 	}
 
-	int get_numerator() const
+	int get_numerator() const noexcept
 	{
 		return _numerator;
 	}
 
-	int get_denominator() const
+	int get_denominator() const noexcept
 	{
 		return _denominator;
 	}
 };
 
-fraction operator+(fraction fr_1, fraction fr_2)
+// F1
+Fraction operator+(const Fraction &frac_1, const Fraction &frac_2)
 {
-	int sum_num = fr_1.get_numerator() * fr_2.get_denominator() + fr_2.get_numerator() * fr_1.get_denominator();
-	int sum_denom = fr_1.get_denominator() * fr_2.get_denominator();
-
-	fraction result(sum_num, sum_denom);
-	return result;
+	return {frac_1.get_numerator() * frac_2.get_denominator() + frac_2.get_numerator() * frac_1.get_denominator(),
+			frac_1.get_denominator() * frac_2.get_denominator()};
 }
 
-fraction operator*(fraction fr_1, fraction fr_2)
+// F1
+Fraction operator-(const Fraction &frac_1, const Fraction &frac_2)
 {
-	int mul_num = fr_1.get_numerator() * fr_2.get_numerator();
-	int mul_denom = fr_1.get_denominator() * fr_2.get_denominator();
-
-	fraction result(mul_num, mul_denom);
-	return result;
+	return {frac_1.get_numerator() * frac_2.get_denominator() - frac_2.get_numerator() * frac_1.get_denominator(),
+			frac_1.get_denominator() * frac_2.get_denominator()};
 }
 
-std::string to_string(fraction fr)
+// F2
+Fraction operator*(const Fraction &frac_1, const Fraction &frac_2)
 {
-	return std::format("{}/{}", fr.get_numerator(), fr.get_denominator());
+	return {frac_1.get_numerator() * frac_2.get_numerator(),
+			frac_1.get_denominator() * frac_2.get_denominator()};
+}
+
+// F3
+std::string to_string(const Fraction &frac)
+{
+	return std::format("{}/{}", frac.get_numerator(), frac.get_denominator());
 }
 
 int main()
 {
-	int n = 0;
-	int d = 1;
-	std::println("введите дробь 1: ");
-	std::cin >> n >> d;
-	fraction fr_1(n, d);
-	std::println("введите дробь 2: ");
-	std::cin >> n >> d;
-	fraction fr_2(n, d);
-	std::println("выберите действие (+ или *): ");
-	std::string op;
-	std::cin >> op;
-	if (op == "+")
-	{
-		fraction res = fr_1 + fr_2;
-		std::println("{}+{}={}", to_string(fr_1), to_string(fr_2), to_string(res));
-	}
-	else if (op == "*")
-	{
-		fraction res = fr_1 * fr_2;
-		std::println("{}*{}={}", to_string(fr_1), to_string(fr_2), to_string(res));
-	}
+	std::println("Демонстрация работы с классом Fraction\n");
+
+	int n1 = 0;
+	int d1 = 0;
+	int n2 = 0;
+	int d2 = 0;
+	std::print("Введите числитель и знаменатель первой дроби: ");
+	std::cin >> n1 >> d1;
+	Fraction f1{n1, d1};
+
+	std::print("Введите числитель и знаменатель второй дроби: ");
+	std::cin >> n2 >> d2;
+	Fraction f2{n2, d2};
+
+	std::println("\nПервая дробь : {}", to_string(f1));
+	std::println("Вторая дробь: {}", to_string(f2));
+	std::println("Сложение    : {}", to_string(f1 + f2));
+	std::println("Вычитание   : {}", to_string(f1 - f2));
+	std::println("Умножение   : {}", to_string(f1 * f2));
 
 	return 0;
 }
