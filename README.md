3437c600dac4541f6f19fc02c8d6fa286eddef88
Dauxdu <dauxdu@yandex.ru>
2026-05-23 17:38:52 +0400

    test

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # oop-2026
+
+## Установка vcpkg
+
+Документация:
+[Microsoft vcpkg Getting Started](https://learn.microsoft.com/en-us/vcpkg/get_started/get-started-vs?utm_source=chatgpt.com)
+
+### 1. Клонирование и установка
+
+```bash
+git clone https://github.com/microsoft/vcpkg.git
+cd vcpkg
+./bootstrap-vcpkg.sh -disableMetrics
+```
+
+### 2. Добавление `VCPKG_ROOT`
+
+Linux/macOS:
+
+```bash
+echo 'export VCPKG_ROOT="$HOME/vcpkg"' >> ~/.bashrc
+source ~/.bashrc
+```
+
+Windows (PowerShell):
+
+```powershell
+setx VCPKG_ROOT "C:\path\to\vcpkg"
+```
+
+### 3. Создание проекта
+
+```bash
+vcpkg new --application
+```
+
+### 4. Установка зависимостей
+
+```bash
+vcpkg add port boost sfml
+vcpkg install
+```
+
+### 5. Экспорт зависимостей
+
+```bash
+vcpkg export --zip --output-dir="./vcpkg_sdks"
+```
---
+++ practice-10/main.cpp
@@ -0,0 +1,62 @@
+﻿#include <SFML/Graphics.hpp>
+
+import std;
+import api;
+import spiral;
+
+static sf::String make_string(const std::string_view s)
+{
+  return sf::String::fromUtf8(s.begin(), s.end());
+}
+
+struct WindowConfig final
+{
+  std::filesystem::path icon_path{"assets/icon.png"};
+  sf::String title{make_string("Демо")};
+  std::uint32_t style{sf::Style::Titlebar | sf::Style::Close /* | sf::Style::Resize */};
+  sf::Vector2u size{1920, 1080};
+};
+
+struct Config final
+{
+  WindowConfig window;
+  spiral::Config spiral{
+      static_cast<float>(window.size.x),
+      static_cast<float>(window.size.y)};
+};
+
+int main()
+{
+  const Config cfg;
+
+  sf::RenderWindow w{
+      sf::VideoMode(cfg.window.size),
+      cfg.window.title,
+      cfg.window.style};
+  w.setIcon(sf::Image(cfg.window.icon_path));
+  w.setVerticalSyncEnabled(true);
+
+  sf::Clock clock;
+
+  api::Scene scene;
+  scene.drawables.push_back(spiral::make(cfg.spiral));
+
+  while (w.isOpen())
+  {
+    while (const auto e = w.pollEvent())
+    {
+      if (e->is<sf::Event::Closed>())
+      {
+        w.close();
+        break;
+      }
+    }
+
+    w.clear(sf::Color::White);
+
+    scene.update(clock.getElapsedTime().asSeconds());
+    scene.draw(static_cast<sf::RenderTarget *>(&w));
+
+    w.display();
+  }
+}
